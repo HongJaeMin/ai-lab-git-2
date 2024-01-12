ee819bcce806695e5c1133472ffe81c89df34759
HongJaeMin <hjm7392@mail.ulsan.ac.kr>
2024-01-12 17:15:21 +0900

    fix: Rename Function add to adder

--- adder.py
+++ adder.py
@@ -1,8 +1,8 @@
-def two_sum():
+def sum_of_two_nums():
     a = int(input('a = '))
     b = int(input('b = '))
     res = a + b
     print(res)
 
 if __name__=='__main__':
-    two_sum()
+    sum_of_two_nums()
